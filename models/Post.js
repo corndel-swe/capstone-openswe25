e49cf06c3efbf8934005b5778c46e86025b50dcf
Sinth Uthayakumar <sinthuk10@gmail.com>
2025-11-14 09:53:52 +0000

    Post class added and findAll method

---
+++ models/Post.js
@@ -0,0 +1,12 @@
+import db from "../db/index.js";
+
+class Post {
+    static async findAll() {
+        const query = `
+        SELECT id, user_id, title, content, image_url, created_at
+        FROM post
+        `;
+        const rows = await db.raw(query);
+        return rows;
+    }
+}
