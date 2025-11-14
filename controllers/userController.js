c8846495d8ecfecb5a0b04322f19f99f6e80df5f
David Taylor <david.taylor@necsws.com>
2025-11-14 17:13:59 +0000

    error handling using next

--- controllers/userController.js
+++ controllers/userController.js
@@ -16,16 +16,16 @@ export const getUsers = async (req, res, next) => {
     }
 }
 
-export const createUser = async (req, res) =>{
-    try{
-        const {username, fullname, email, password_hash, imageURL} = req.body
+export const createUser = async (req, res) => {
+    try {
+        const { username, fullname, email, password_hash, imageURL } = req.body
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/;
 
 
 
 
-        if(!username || !fullname || !email || !password_hash || !imageURL){
+        if (!username || !fullname || !email || !password_hash || !imageURL) {
             throw new AppError('Missing required fields: Username, full name, email address, password and an image must all be provided', 400)
         }
 
@@ -33,24 +33,15 @@ export const createUser = async (req, res) =>{
             throw new AppError('Invalid email format', 400);
         }
 
-    
+
         if (!passwordRegex.test(password_hash)) {
             throw new AppError('Password must be at least 8 characters and include 1 uppercase, 1 lowercase, 1 number, and 1 symbol.', 400);
         }
 
-        const newUser = await User.createNewUser({username, fullname, email, password_hash, imageURL})
+        const newUser = await User.createNewUser({ username, fullname, email, password_hash, imageURL })
         res.status(201).send(newUser)
 
-    }catch(err){
-
-        if(err instanceof AppError){
-            res.status(err.code).send({msg: err.message})
-        } else if(err.errno === 19){
-            res.status(400).send({msg: 'Email or username is already associated with an account.' })
-        }
-        else {
-
-            res.status(500).send({msg: 'Something went wrong'})
-        }
-}
+    } catch (err) {
+        next(err)
+    }
 }
--- errors/errorHandling.js
+++ errors/errorHandling.js
@@ -4,7 +4,10 @@ import AppError from "../models/AppError.js"
 export const handleErrors = (err, req, res, next) => {
     if (err instanceof AppError) {
         res.status(err.code).send({ msg: err.message })
-    } else {
+    } else if (err.errno === 19) {
+        res.status(400).send({ msg: 'Email or username is already associated with an account.' })
+    }
+    else {
         res.status(500).send({ msg: 'Something went wrong' })
     }
 }
