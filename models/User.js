445d30b5d483d5db884504df03bd9982587fdbbe
Chuks Alichukwu <alichukwucp@gmail.com>
2025-11-14 15:44:10 +0000

    complete create new user endpoint

--- models/User.js
+++ models/User.js
@@ -6,6 +6,18 @@ class User {
     const results = await db.raw(`SELECT username, imageURL FROM user`)
     return results
     }
+
+    static async createNewUser({username, fullname, email, password_hash, imageURL}){
+
+        const query = `
+        INSERT INTO user
+        (username, fullname, email, password_hash, imageURL)
+        VALUES (?, ?, ?, ?, ?)
+        RETURNING*;
+        `
+        const results = await db.raw(query, [username, fullname, email, password_hash, imageURL])
+        return results[0]
+    }
 }
 
   
