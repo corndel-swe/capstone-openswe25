1f2d371f9e22869e922e7170389d783c675dcb42
David Taylor <david.taylor@necsws.com>
2025-11-14 10:44:39 +0000

    console log removed

--- db/index.js
+++ db/index.js
@@ -3,8 +3,6 @@ import { fileURLToPath } from 'url'
 
 const uri = new URL('./db.sqlite', import.meta.url)
 
-console.log(fileURLToPath(uri))
-
 const db = knex({
   client: 'sqlite3',
   useNullAsDefault: true,
