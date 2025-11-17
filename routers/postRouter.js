f8d9c66f913aa1245f54e352e687c557c14072c0
David Taylor <david.taylor@necsws.com>
2025-11-17 10:52:38 +0000

    like post complete

--- routers/postRouter.js
+++ routers/postRouter.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { getPostById, getAllPosts, postNewPost } from "../controllers/postControllers.js";
+import { getPostById, getAllPosts, postNewPost, postLike } from "../controllers/postControllers.js";
 
 const postRouter = new Router()
 
 postRouter.get('/:id', getPostById)
 postRouter.get('/', getAllPosts)
 postRouter.post('/', postNewPost)
+postRouter.post('/:id/like', postLike)
 
 export default postRouter
 
