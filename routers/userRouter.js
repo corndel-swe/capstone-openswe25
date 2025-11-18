653d8bdae5963d1897aeba95439fa9429e84764f
Chuks Alichukwu <alichukwucp@gmail.com>
2025-11-18 17:10:19 +0000

    Add header partial and authorisation middleware

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { getUsers, createUser, loginUser, getRegisterPage, getLoginPage} from "../controllers/userController.js";
+import { getUsers, createUser, loginUser, getRegisterPage, getLoginPage, logoutUser} from "../controllers/userController.js";
+import { redirectIfLoggedIn, requireAuth} from "../controllers/auth.js";
 
 const userRouter = Router()
-userRouter.get('/', getUsers)
-userRouter.get('/register', getRegisterPage)
-userRouter.get('/login', getLoginPage)
+userRouter.get('/', requireAuth, getUsers)
+userRouter.get('/register', redirectIfLoggedIn, getRegisterPage)
+userRouter.get('/login', redirectIfLoggedIn, getLoginPage)
 userRouter.post('/register', createUser)
 userRouter.post('/login', loginUser)
+userRouter.get('/logout',requireAuth, logoutUser)
 
 export default userRouter
