83a79d3e7fb536216dc5bc111d924b0c750f090a
Chuks Alichukwu <alichukwucp@gmail.com>
2025-11-18 10:11:44 +0000

    Add upload image button and save to public

---
+++ public/js/script.js
@@ -0,0 +1,13 @@
+
+const fileInput = document.querySelector('#profilePicture');
+const hiddenInput = document.querySelector('#imageBase64');
+
+fileInput.addEventListener('change', function () {
+    const reader = new FileReader();
+    
+    reader.onload = () => {
+        hiddenInput.value = reader.result; // base64 string stored here
+    };
+    console.log(hiddenInput.value)
+    reader.readAsDataURL(this.files[0]);
+});
